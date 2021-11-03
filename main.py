cee26257e335c044b25c15472bfafa646553d617
Samirkhairati <34453746+Samirkhairati@users.noreply.github.com>
2021-11-03 13:36:37 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+def mint(t):
+	h,m = str(t//60),str(t%60)
+	if int(h)>=24:
+		h = '0'
+	if int(h)<10:
+		h = '0'+h
+	if int(m)<10:
+		m = '0'+m
+	return h+':'+m
+	
+# start time
+a = 7*60+45
+# first slot length
+x = 90
+# slope
+d = -5
+# number of slots
+n = 12
+# minutes per break
+b = 10
+
+f = x
+for i in range(n):
+	print(mint(a)+'-'+mint(a+f)+' →')
+	f+=d
+	a+=f+b
+s = (n/2)*(2*x+(n-1)*d)//60
+
+print('\nHours Studied: ',s)
+print('Total Hours: ',s+b*n//60)
